9bbe2b0723365c366506121f00874d53887dd200
makemango <48336796+makemango@users.noreply.github.com>
2019-11-16 23:56:46 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -655,14 +655,7 @@ function xmas_calendar()
 }
 add_shortcode('xmas', 'xmas_calendar');
 
-function generate_options_css() {
-  $ss_dir = get_stylesheet_directory();
-  ob_start(); // Capture all output into buffer
-  require($ss_dir . '/assets/css/custom-styles.php'); // Grab the custom-style.php file
-  $css = ob_get_clean(); // Store output in a variable, then flush the buffer
-  file_put_contents($ss_dir . '/assets/css/custom-styles.css', $css, LOCK_EX); // Save it as a css file
-}
-add_action( 'acf/save_post', 'generate_options_css', 20 ); //Parse the output and write the CSS file on post save (thanks Esmail Ebrahimi)
+
 
 
 ?>
